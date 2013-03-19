05d08d4d541a28df753252bf098241ae29247388
Kirby Fike <kirbyfike@gmail.com>
2013-03-19 08:29:30 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SiteScraper::Application.config.secret_token = '5a5e7af3a729cc20aac1e9a0bd06bec3ed2ed2cc167ef81147744a16dbbc4007f141cf0db7951469db747df52c81711b965c9429ce072e8c48f672be20a88c28'
